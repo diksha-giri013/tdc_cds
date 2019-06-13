bd3ab1a0bf41a6ebdb5bca56e40a7635e9e9ccea
diksha-giri013 <diksha.giri013@gmail.com>
2019-06-13 21:44:27 +0530

    [Mod] Controls [Add] Transitions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,16 @@
 import React, {useState, useEffect} from 'react';
+import { CSSTransition } from 'react-transition-group';
 //import logo from './logo.svg';
 import './App.css';
-//import demo from './demo.png';
-//import prev from './res/svg/left-arrow.svg';
-//import next from './res/svg/right-arrow.svg';
 import {courses} from './res/demo/res';
+
 
 const styles = {
   disabled: {
-    display: 'none'
+    visibility: 'hidden'
   },
   enabled: {
-    display: 'initial'
+    opacaity: 1
   }
 }
 
@@ -28,35 +27,43 @@ function App() {
   },[mod]); 
   //component for reader content
   const Reader = () => {
+    const [inProp, setInProp] = useState(false);
+    useEffect(()=>{
+      setInProp(true);
+    },[inProp] )
     return(
+      <CSSTransition in={inProp} appear timeout={400} classNames="my-node">
       <div className="reader maxHeight">
+        {inProp ?
         <div id="readerText" className="col readerText"> 
-          <h2 className="fontOswald">{mod.heading[0]} <span className="fontOswald highlight">{mod.heading[1]}</span> </h2>
+          <h2 className="fontOswald">{mod.heading[0]} <span className="fontOswald highlight">{mod.heading[1]}</span> </h2> 
           <p className="fontOswald metaData">By {mod.metaData[1]} &nbsp; &nbsp;<span className="date">{mod.metaData[0]}</span></p> 
           <h3 className="subHeading">{page}. &nbsp;{mod.sectionData.subHeading[page]}</h3>
           {mod.sectionData.content[page].map((para, i) => <p key={i}>{para}</p>)}
           <Control/>
-        </div>
+        </div>: 0}
         <div className="col readerMedia"><img width="100%" src={mod.sectionData.media[page]} alt=""/></div>
-      </div>
+      </div>   
+      </CSSTransition>
     );
   }
-
-  //Hook for reader control bar
-  const size = Object.keys(mod.sectionData.content).length;
-  let initialButton = {prev : styles.disabled,next: styles.enabled};
-  const [inputPage, setInputPage] = useState(page);
-  const [button, setButton] = useState(initialButton);
-  useEffect(() => {
-    if(page>1)
-      setButton({prev: styles.enabled, next: styles.enabled});
-    if(page===1)
-      setButton({prev: styles.disabled, next: styles.enabled});  
-    if(page===size)
-      setButton({prev: styles.enabled, next: styles.disabled});  
-  },[page, size]);
+  
   //component for reader control bar
+  const size = Object.keys(mod.sectionData.content).length;
   const Control = () => {
+    //Hook for reader control bar
+    let initialButton = {prev : true, next: false};
+    const [isDisabled, setDisabled] = useState(initialButton);
+    useEffect(() => {
+      if(page>1)
+        setDisabled({prev: false});
+      if(page===1)
+        setDisabled({prev: true});
+      if(page===size)
+        setDisabled({next:true});
+    },[isDisabled]);
+    //Hook for indicator form field
+    const [inputPage, setInputPage] = useState(page);
     const handleChange = (e) => {
       setInputPage(e.target.value);
     }
@@ -65,26 +72,29 @@ function App() {
       setPage(parseInt(inputPage));
     }
     return(
-      <div className="controls">
-        <button style={button.prev} onClick ={() => setPage(page-1)} className="button"> ◀ </button>
-          <form onSubmit={handleSubmit}><input className="indicator" onChange={handleChange}value={inputPage}/></form>
-        <button style={button.next} onClick ={() => setPage(page+1)}className="button"> ▶ </button>
+      <div className="flex">
+        <div className="controls">
+          {isDisabled.prev ? <button disabled onClick ={() => setPage(page-1)} style={styles.disabled} className="button"> ◀ </button> : <button onClick ={() => setPage(page-1)} className="button"> ◀ </button>}
+          <form onSubmit={handleSubmit}><input className="indicator" onChange={e => handleChange(e)} value={inputPage} /> <label className="label">/ {size}</label></form>
+          {isDisabled.next ? <button disabled onClick ={() => setPage(page+1)} style={styles.disabled} className="button"> ▶ </button> : <button onClick ={() => setPage(page+1)}className="button"> ▶ </button> }
+        </div>
       </div>
     );
   }
-
-  //hook for progressBar
-  const [progress, setProgress] = useState(page/size);
-  useEffect(() => {
-    setProgress(page/size);
-  },[page, size]);
+  
   //component for progress bar
   const ProgressBar = () => {
+    //hook for progressBar
+    const [progress, setProgress] = useState(page/size);
+    useEffect(() => {
+    setProgress(page/size);
+    },[progress]);
+
     const styleProgress ={
       width: `calc(${progress}*100%)`,
       borderBottom: '0.25rem solid lightgreen'
     }
-    return(<div style={styleProgress}></div>);
+    return(<CSSTransition><div style={styleProgress}></div></CSSTransition>);
   }
 
   //component for Nav  
